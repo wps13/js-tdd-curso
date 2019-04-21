a7fe509f5a92c71a413d752f4a617bdf7c7aa111
wps13 <willanepaiva@hotmail.com>
2019-04-21 15:06:07 -0300

    Install babel

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
-  extends: ["airbnb-base", "plugin:prettier/recommended"]
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  rules: {
+    "no-unused-expressions": 0,
+    "func-names ": 0,
+    "no-undef": 0
+  }
 };
