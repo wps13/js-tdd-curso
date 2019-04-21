3366a5b1469a7b1ebb91c988911d753a3f029321
wps13 <willanepaiva@hotmail.com>
2019-04-21 15:09:08 -0300

    Fix tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,6 +1,6 @@
 /* eslint-disable func-names */
 import { expect } from "chai";
-import calc from "../src/main";
+import calc, { sum, sub, mult, div } from "../src/main";
 
 describe("Calc", () => {
   describe("Smoke test", () => {
@@ -8,43 +8,43 @@ describe("Calc", () => {
       expect(calc).to.exist;
     });
     it("should exist the method sum", () => {
-      expect(calc.sum).to.exist;
-      // expect(calc.sum).to.be.a.;
+      expect(sum).to.exist;
+      // expect(sum).to.be.a.;
     });
     it("should exist the method sub", () => {
-      expect(calc.sum).to.exist;
-      // expect(calc.sum).to.be.a.;
+      expect(sum).to.exist;
+      // expect(sum).to.be.a.;
     });
     it("should exist the method mult", () => {
-      expect(calc.sum).to.exist;
-      // expect(calc.sum).to.be.a.;
+      expect(sum).to.exist;
+      // expect(sum).to.be.a.;
     });
     it("should exist the method div", () => {
-      expect(calc.sum).to.exist;
-      // expect(calc.sum).to.be.a.;
+      expect(sum).to.exist;
+      // expect(sum).to.be.a.;
     });
   });
   describe("Sum", () => {
     it("Should return 4 when sum(2,2)", () => {
-      expect(calc.sum(2, 2)).to.be.equal(4);
+      expect(sum(2, 2)).to.be.equal(4);
     });
   });
   describe("Sub", () => {
     it("Should return 4 when sub(6,2)", () => {
-      expect(calc.sub(6, 2)).to.be.equal(4);
+      expect(sub(6, 2)).to.be.equal(4);
     });
     it("Should return -4 when sub(6,10)", () => {
-      expect(calc.sub(6, 10)).to.be.equal(-4);
+      expect(sub(6, 10)).to.be.equal(-4);
     });
   });
   describe("Mult", () => {
     it("Should return 4 when mult(2,2)", () => {
-      expect(calc.mult(2, 2)).to.be.equal(4);
+      expect(mult(2, 2)).to.be.equal(4);
     });
   });
   describe("Div", () => {
     it("Should return 4 when div(8,2)", () => {
-      expect(calc.div(8, 2)).to.be.equal(4);
+      expect(div(8, 2)).to.be.equal(4);
     });
   });
 });
