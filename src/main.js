0e4cc52417de8be154ce0a65b4e8f7e4a9814cd4
wps13 <willanepaiva@hotmail.com>
2019-04-21 15:13:33 -0300

    fix main export

--- src/main.js
+++ src/main.js
@@ -1,17 +1,17 @@
-module.exports = {
-  sum(num1, num2) {
-    return num1 + num2;
-  },
-  sub(num1, num2) {
-    return num1 - num2;
-  },
-  mult(num1, num2) {
-    return num1 * num2;
-  },
-  div(num1, num2) {
-    return num1 / num2;
-  }
-};
+// module.exports = {
+//   sum(num1, num2) {
+//     return num1 + num2;
+//   },
+//   sub(num1, num2) {
+//     return num1 - num2;
+//   },
+//   mult(num1, num2) {
+//     return num1 * num2;
+//   },
+//   div(num1, num2) {
+//     return num1 / num2;
+//   }
+// };
 
 const sum = (num1, num2) => num1 + num2;
 const sub = (num1, num2) => num1 - num2;
